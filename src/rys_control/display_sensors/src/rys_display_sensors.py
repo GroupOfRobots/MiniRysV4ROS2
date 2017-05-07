acc05378dd02076e9d13ffb9b3c4cb51c5f4bc5f
mjbogusz <mjbogusz+github@gmail.com>
2017-05-07 19:57:54 +0200

    rys_control: display_sensors: switched to ImuRoll, added timeout value to spin_once to prevent memory leak

--- src/rys_control/display_sensors/src/rys_display_sensors.py
+++ src/rys_control/display_sensors/src/rys_display_sensors.py
@@ -1,7 +1,7 @@
 import sys
 import rclpy
 from rclpy.qos import qos_profile_sensor_data
-from rys_messages.msg import ImuYawPitchRoll
+from rys_messages.msg import ImuRoll
 
 def sensorsReadingCallback(msg):
 	print('Received: %f' % (msg.roll))
@@ -13,12 +13,13 @@ def main(args = None):
 	rclpy.init(args)
 
 	node = rclpy.create_node('rys_display_sensors')
-	sub = node.create_subscription(ImuYawPitchRoll, 'rys_imu', sensorsReadingCallback, qos_profile_sensor_data)
-	# prevent unused warning
+	sub = node.create_subscription(ImuRoll, 'rys_imu', sensorsReadingCallback, qos_profile_sensor_data)
+	# prevent unused variable warning
 	assert sub
 
 	while rclpy.ok():
-		rclpy.spin_once(node)
+		# Timeout of 1 due to RCLPY memory leak bug (https://github.com/ros2/rclpy/issues/74, there is already a PR that fixes it, PR#79)
+		rclpy.spin_once(node, 1.0)
 
 if __name__ == '__main__':
 	main()
