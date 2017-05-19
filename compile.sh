55a6dc1cd37270ac1492b929bbb97db71d14192a
mjbogusz <mjbogusz+github@gmail.com>
2017-05-19 15:04:23 +0200

    compile.sh: added checking for ament availability

--- compile.sh
+++ compile.sh
@@ -8,6 +8,13 @@ ROBOT_PKGS="${ROBOT_PKGS} rys_sensor_imu rys_sensor_sonars "
 CONTROL_PKGS="rys_display_sensors "
 CONTROL_PKGS="${CONTROL_PKGS} rys_remote "
 
+# Check whether ament is available
+command -v ament > /dev/null 2>&1;
+if [ $? -ne 0 ] ; then
+	echo "Ament not found. Install ROS2 first (try 'ros2_install')." >&2
+	exit 1
+fi
+
 if [ -z $1 ] ; then
 	ament build ${ARGS}
 	exit
