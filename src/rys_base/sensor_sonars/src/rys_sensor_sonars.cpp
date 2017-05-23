9ac14679b93a475ba825b9a67dc63c6b6c3f2948
mjbogusz <mjbogusz+github@gmail.com>
2017-05-23 21:05:32 +0200

    rys_sensor_sonars: changed message type from Sonars to Ranges

--- src/rys_base/sensor_sonars/src/rys_sensor_sonars.cpp
+++ src/rys_base/sensor_sonars/src/rys_sensor_sonars.cpp
@@ -2,15 +2,15 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "Sonars.h"
-#include "rys_interfaces/msg/sonars.hpp"
+#include "rys_interfaces/msg/ranges.hpp"
 
 int main(int argc, char * argv[]) {
 	std::cout << "Initializing ROS...\n";
 	rclcpp::init(argc, argv);
-	auto node = rclcpp::node::Node::make_shared("rys_node_motors_controller");
-	auto sensorsPublisher = node->create_publisher<rys_interfaces::msg::Sonars>("rys_sensor_sonars", rmw_qos_profile_sensor_data);
+	auto node = rclcpp::node::Node::make_shared("rys_node_sensor_sonars");
+	auto sensorsPublisher = node->create_publisher<rys_interfaces::msg::Ranges>("rys_sensor_sonars", rmw_qos_profile_sensor_data);
 
-	auto message = std::make_shared<rys_interfaces::msg::Sonars>();
+	auto message = std::make_shared<rys_interfaces::msg::Ranges>();
 
 	std::cout << "Initializing sonars...\n";
 	Sonars sonars;
@@ -28,6 +28,8 @@ int main(int argc, char * argv[]) {
 			message->front = front;
 			message->back = back;
 			message->top = top;
+			message->left = -1;
+			message->right = -1;
 
 			sensorsPublisher->publish(message);
 		} catch (std::string & error) {
