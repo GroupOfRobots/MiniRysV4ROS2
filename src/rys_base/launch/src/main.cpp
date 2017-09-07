d95ddde7c4f1844ac1bfc2f3ff4237aeecfdd34c
mjbogusz <mjbogusz+github@gmail.com>
2017-09-07 19:15:01 +0200

    rys_launch: * added IMU node * added using compile options from common cmake module * changed node header includes to module-based (from directory-based)

--- src/rys_base/launch/src/main.cpp
+++ src/rys_base/launch/src/main.cpp
@@ -2,8 +2,9 @@
 #include <memory>
 #include "rclcpp/rclcpp.hpp"
 
-#include "../../motors_controller/src/MotorsControllerNode.hpp"
-#include "../../sensor_dwm1000/src/DWMNode.hpp"
+#include "rys_motors_controller/MotorsControllerNode.hpp"
+#include "rys_sensor_dwm1000/DWMNode.hpp"
+#include "rys_sensor_imu/IMUNode.hpp"
 
 using namespace std::chrono_literals;
 
@@ -14,8 +15,10 @@ int main(int argc, char * argv[]) {
 
 	auto motorsNode = std::make_shared<MotorsControllerNode>("rys_node_motors_controller", 10ms);
 	auto dwmNode = std::make_shared<DWMNode>("rys_node_sensor_dwm1000", "rys_sensor_dwm1000", 1000ms);
+	auto imuNode = std::make_shared<IMUNode>("rys_node_sensor_imu", "rys_sensor_imu_roll", "rys_control_imu_calibrate", 10ms, 3000ms);
 	executor.add_node(motorsNode);
 	executor.add_node(dwmNode);
+	executor.add_node(imuNode);
 	executor.spin();
 
 	rclcpp::shutdown();
