8d6a2be815d418d89bdc727853a0a984225afd41
mjbogusz <mjbogusz+github@gmail.com>
2017-10-18 11:31:22 +0200

    sensor_dwm1000: disabled DWM until anchors are working

--- src/rys_base/sensor_dwm1000/src/DWMNode.cpp
+++ src/rys_base/sensor_dwm1000/src/DWMNode.cpp
@@ -5,8 +5,8 @@
 #include "DWMNode.hpp"
 
 DWMNode::DWMNode(const char * nodeName, const char * topicName, std::chrono::milliseconds rate) : rclcpp::Node(nodeName) {
-	this->dwm = new DWM(115, 20);
-	this->dwm->initialize(true);
+	// this->dwm = new DWM(115, 20);
+	// this->dwm->initialize(true);
 
 	this->publisher = this->create_publisher<std_msgs::msg::Float64>(topicName);
 	this->timer = this->create_wall_timer(rate, std::bind(&DWMNode::publishData, this));
@@ -19,8 +19,9 @@ DWMNode::~DWMNode() {
 void DWMNode::publishData() {
 	auto message = std_msgs::msg::Float64();
 
-	message.data = this->dwm->readRange();
-	std::cout << "DWM1000: publishing message with data: " << message.data << std::endl;
+	// message.data = this->dwm->readRange();
+	message.data = 0.1;
+	// std::cout << "DWM1000: publishing message with data: " << message.data << std::endl;
 
 	this->publisher->publish(message);
 }
