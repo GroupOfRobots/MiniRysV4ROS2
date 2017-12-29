9129d5ee0845e8b11d70a5f3497765fd5cde8c4c
mjbogusz <mjbogusz+github@gmail.com>
2017-12-29 03:19:55 +0100

    rys_remote: Mapper: * add sensor threshold (minimum) * adjust sensor x/y shift * fix range reading unit * fix linear equation angle coefficient

--- src/rys_remote/src/Mapper/Mapper.py
+++ src/rys_remote/src/Mapper/Mapper.py
@@ -11,13 +11,14 @@ class Mapper(QThread):
 
 	startPosition = PyKDL.Frame(PyKDL.Rotation.RotZ(math.pi / 2))
 
-	def __init__(self, parent, rosBridge, cellSize = 0.04, mapSize = 2.56):
+	def __init__(self, parent, rosBridge, cellSize = 0.04, mapSize = 2.56, sensorThreshold = 0.04):
 		super().__init__(parent)
 
 		self.updated = True
 
 		self.cellSize = cellSize
 		self.mapSize = mapSize
+		self.sensorThreshold = sensorThreshold
 
 		self.map = QuadMapNode(mapSize, cellSize)
 
@@ -28,15 +29,16 @@ class Mapper(QThread):
 		# Robot is Y-forward-oriented, so it starts with angle=PI/2
 		self.robotPosition = PyKDL.Frame(Mapper.startPosition)
 		self.path = [(0, PyKDL.Frame(self.robotPosition))]
+		self.odoSeq = 0
 
 		# All sensors 'aim' down their their Y axis
 		# [Assuming laying down mode]
 		# top sensor is offset by ~10cm on Y axis from base frame
-		self.topRangeSensorFrame = PyKDL.Frame(PyKDL.Vector(0, 0.1, 0))
+		self.topRangeSensorFrame = PyKDL.Frame(PyKDL.Vector(0, 0.086, 0))
 		# right sensor is offset by half frame width in X and slightly above wheel radius in Y, also rotated
-		self.rightRangeSensorFrame = PyKDL.Frame(PyKDL.Rotation.RotZ(-math.pi / 2), PyKDL.Vector(0.065, 0.06, 0))
+		self.rightRangeSensorFrame = PyKDL.Frame(PyKDL.Rotation.RotZ(-math.pi / 2), PyKDL.Vector(0.059, 0.067, 0))
 		# left sensor symmetrical to right sensor
-		self.leftRangeSensorFrame = PyKDL.Frame(PyKDL.Rotation.RotZ(math.pi / 2), PyKDL.Vector(-0.065, 0.06, 0))
+		self.leftRangeSensorFrame = PyKDL.Frame(PyKDL.Rotation.RotZ(math.pi / 2), PyKDL.Vector(-0.059, 0.067, 0))
 
 		rosBridge.odometryChanged.connect(self.odometryHandler)
 		rosBridge.rangesChanged.connect(self.rangeReadingsHandler)
@@ -54,7 +56,9 @@ class Mapper(QThread):
 		# Apply odometry transform onto robotPosition frame
 		self.robotPosition = self.robotPosition * poseFrame
 
-		print('time: %f heading: %f' % (message.header.stamp.sec + message.header.stamp.nanosec * 0.000000001, self.robotPosition.M.GetRPY()[2]))
+		# Debug print, enable if odometry frames are suspected to be lost
+		# print('s: %d h: %f' % (self.odoSeq, self.robotPosition.M.GetRPY()[2]))
+		self.odoSeq = self.odoSeq + 1
 
 		# Save the frame and time to the path
 		poseTime = float(message.header.stamp.sec) + float(message.header.stamp.nanosec) / 1000000000
@@ -65,34 +69,45 @@ class Mapper(QThread):
 
 	def rangeReadingsHandler(self, message):
 		# Left
-		# First, apply sensor's transform to the current odometry position
-		leftSensorPosition = self.robotPosition * self.leftRangeSensorFrame
-		# Then, calculate linear function parameters
-		leftSensorX0 = leftSensorPosition.p.x()
-		leftSensorX1 = leftSensorX0 + math.cos(leftSensorPosition.M.GetRPY()[2]) * message.left
-		leftSensorY0 = leftSensorPosition.p.y()
-		leftSensorA = leftSensorPosition.M.GetRPY()[2]
-		leftSensorB = leftSensorY0 - leftSensorA * leftSensorX0
-		# Then, pass calculated parameters to quadmap
-		self.map.addScan(leftSensorA, leftSensorB, leftSensorX0, leftSensorX1)
+		# First, decide whether to interpret the reading at all
+		leftSensorRange = message.left * 0.001
+		if leftSensorRange > self.sensorThreshold:
+			# Then, apply current odometry transform to the frame of left sensor
+			leftSensorPosition = self.leftRangeSensorFrame * self.robotPosition
+			# Then, calculate linear function parameters
+			leftSensorX0 = leftSensorPosition.p.x()
+			leftSensorY0 = leftSensorPosition.p.y()
+			leftSensorAngle = leftSensorPosition.M.GetRPY()[2]
+			# Ranges are in millimeters, our calculation - in meters
+			leftSensorX1 = leftSensorX0 + math.cos(leftSensorAngle) * leftSensorRange
+			leftSensorA = math.tan(leftSensorAngle)
+			leftSensorB = leftSensorY0 - leftSensorA * leftSensorX0
+			# Then, pass calculated parameters to quadmap
+			self.map.addScan(leftSensorA, leftSensorB, leftSensorX0, leftSensorX1)
 
 		# Right, same thing
-		rightSensorPosition = self.robotPosition * self.rightRangeSensorFrame
-		rightSensorX0 = rightSensorPosition.p.x()
-		rightSensorX1 = rightSensorX0 + math.cos(rightSensorPosition.M.GetRPY()[2]) * message.right
-		rightSensorY0 = rightSensorPosition.p.y()
-		rightSensorA = rightSensorPosition.M.GetRPY()[2]
-		rightSensorB = rightSensorY0 - rightSensorA * rightSensorX0
-		self.map.addScan(rightSensorA, rightSensorB, rightSensorX0, rightSensorX1)
+		rightSensorRange = message.right * 0.001
+		if rightSensorRange > self.sensorThreshold:
+			rightSensorPosition = self.rightRangeSensorFrame * self.robotPosition
+			rightSensorX0 = rightSensorPosition.p.x()
+			rightSensorY0 = rightSensorPosition.p.y()
+			rightSensorAngle = rightSensorPosition.M.GetRPY()[2]
+			rightSensorX1 = rightSensorX0 + math.cos(rightSensorAngle) * rightSensorRange
+			rightSensorA = math.tan(rightSensorAngle)
+			rightSensorB = rightSensorY0 - rightSensorA * rightSensorX0
+			self.map.addScan(rightSensorA, rightSensorB, rightSensorX0, rightSensorX1)
 
 		# Top
-		topSensorPosition = self.robotPosition * self.topRangeSensorFrame
-		topSensorX0 = topSensorPosition.p.x()
-		topSensorX1 = topSensorX0 + math.cos(topSensorPosition.M.GetRPY()[2]) * message.top
-		topSensorY0 = topSensorPosition.p.y()
-		topSensorA = topSensorPosition.M.GetRPY()[2]
-		topSensorB = topSensorY0 - topSensorA * topSensorX0
-		self.map.addScan(topSensorA, topSensorB, topSensorX0, topSensorX1)
+		topSensorRange = message.top * 0.001
+		if topSensorRange > self.sensorThreshold:
+			topSensorPosition = self.topRangeSensorFrame * self.robotPosition
+			topSensorX0 = topSensorPosition.p.x()
+			topSensorY0 = topSensorPosition.p.y()
+			topSensorAngle = topSensorPosition.M.GetRPY()[2]
+			topSensorX1 = topSensorX0 + math.cos(topSensorAngle) * topSensorRange
+			topSensorA = math.tan(topSensorAngle)
+			topSensorB = topSensorY0 - topSensorA * topSensorX0
+			self.map.addScan(topSensorA, topSensorB, topSensorX0, topSensorX1)
 
 		self.updated = True
 
