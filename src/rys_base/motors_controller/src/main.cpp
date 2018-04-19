06bf84fd971ee0113f106f04b9f7cff009d63f3a
Dangield <dan.gield@gmail.com>
2018-04-19 15:19:31 +0200

    motors: temporary variable added

--- src/rys_base/motors_controller/src/main.cpp
+++ src/rys_base/motors_controller/src/main.cpp
@@ -11,18 +11,21 @@ int main(int argc, char * argv[]) {
 
 	double wheelRadius = 0.056;
 	double baseWidth = 0.135;
+	float temporaryValue = 0.0;
 
 	for (int i = 2; i <= argc; i+=2) {
 		if (!std::strcmp(argv[i-1], "-r")){
 			wheelRadius = atof(argv[i]);
 		} else if (!std::strcmp(argv[i-1], "-w")){
 			baseWidth = atof(argv[i]);
+		} else if (!std::strcmp(argv[i-1], "-t")){
+			temporaryValue = atof(argv[i]);
 		}
 	}
 
 	std::cout << "[MOTORS] wheelRadius = " << wheelRadius << "; baseWidth = " << baseWidth << ";\n";
 
-	auto node = std::make_shared<MotorsControllerNode>("rys", "motors_controller", 20ms, wheelRadius, baseWidth);
+	auto node = std::make_shared<MotorsControllerNode>("rys", "motors_controller", 10ms, wheelRadius, baseWidth, 0, temporaryValue);
 	rclcpp::spin(node);
 	rclcpp::shutdown();
 
