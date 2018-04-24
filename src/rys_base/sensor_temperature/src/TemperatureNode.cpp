feebcd5681e57c459b12881d65ee6bf8523d0aeb
mjbogusz <mjbogusz+github@gmail.com>
2018-04-24 19:07:28 +0200

    sensor_temperature: removed sleep by splitting reading sensor data and publishing it every 5 times

--- src/rys_base/sensor_temperature/src/TemperatureNode.cpp
+++ src/rys_base/sensor_temperature/src/TemperatureNode.cpp
@@ -33,15 +33,21 @@ TemperatureNode::TemperatureNode(
 	const uint8_t inputNumber,
 	const float coefficient,
 	const float criticalLevel,
-	const float hysteresis
+	const float hysteresis,
+	const int readings
 ) : rclcpp::Node(nodeName, robotName, useIPC) {
 	this->filename = std::string("/sys/devices/platform/ocp/44e0d000.tscadc/TI-am335x-adc/iio:device0/in_voltage") + std::to_string(inputNumber) + std::string("_raw");
 	this->coefficient = coefficient;
 	this->criticalLevel = criticalLevel;
 	this->hysteresis = hysteresis;
+	this->readings = readings;
+
+	this->isCritical = true;
+	this->currentReadings = 0;
+	this->voltageSum = 0;
 
 	this->publisher = this->create_publisher<rys_interfaces::msg::TemperatureStatus>("/" + robotName + "/sensor/temperature", rmw_qos_profile_default);
-	this->timer = this->create_wall_timer(rate, std::bind(&TemperatureNode::publishData, this));
+	this->timer = this->create_wall_timer(rate/5, std::bind(&TemperatureNode::readData, this));
 	std::cout << "[TEMP] Node ready\n";
 
 	setRTPriority();
@@ -49,30 +55,33 @@ TemperatureNode::TemperatureNode(
 
 TemperatureNode::~TemperatureNode() {}
 
-void TemperatureNode::publishData() {
+void TemperatureNode::readData() {
+	std::ifstream file;
+	int rawValue = 0;
+
+	file.open(this->filename, std::ios::in);
+	file >> rawValue;
+	file.close();
+	this->voltageSum += static_cast<float>(rawValue) / coefficient;
+	this->currentReadings++;
+
+	if (this->currentReadings == this->readings) {
+		this->currentReadings = 0;
+
+		// Divide sum by number of readings and multiply by 100
+		// The sensor is 10mV/C, so to convert volts to degrees
+		float avgTemperature = (this->voltageSum / this->readings) * 100;
+		this->publishData(avgTemperature);
+	}
+}
+
+void TemperatureNode::publishData(float temperature) {
 	auto message = std::make_shared<rys_interfaces::msg::TemperatureStatus>();
 
 	message->header.stamp = this->now();
 	message->header.frame_id = "LM35";
 
-	const int readings = 5;
-
-	std::ifstream file;
-	float voltageSum = 0;
-	int rawValue = 0;
-
-	for (int i = 0; i < readings; ++i) {
-		file.open(this->filename, std::ios::in);
-		file >> rawValue;
-		file.close();
-		voltageSum += static_cast<float>(rawValue) / coefficient;
-		rclcpp::sleep_for(100ms);
-	}
-
-	// Divide sum by number of readings and multiply by 100
-	// The sensor is 10mV/C, so to convert volts to degrees
-	message->temperature = (voltageSum / readings) * 100;
-
+	message->temperature = temperature;
 	if (message->temperature > this->criticalLevel) {
 		this->isCritical = true;
 	} else if ((message->temperature + hysteresis) < this->criticalLevel) {
