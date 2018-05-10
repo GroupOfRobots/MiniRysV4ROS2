c44b9624018a69ffb26561a47c6349704a344d55
Dangield <dan.gield@gmail.com>
2018-05-10 19:28:56 +0200

    rys_launch: temporarily commented everything in both launches apart from motor_controller and imu nodes, added setting RT priority to multithread launch

--- src/rys_base/launch/src/main.cpp
+++ src/rys_base/launch/src/main.cpp
@@ -6,10 +6,10 @@
 #include "rclcpp/rclcpp.hpp"
 
 #include "rys_motors_controller/MotorsControllerNode.hpp"
-#include "rys_sensor_battery/BatteryNode.hpp"
+// #include "rys_sensor_battery/BatteryNode.hpp"
 // #include "rys_sensor_dwm1000/DWMNode.hpp"
 #include "rys_sensor_imu/IMUNode.hpp"
-#include "rys_sensor_temperature/TemperatureNode.hpp"
+// #include "rys_sensor_temperature/TemperatureNode.hpp"
 // #include "rys_sensor_ranges/RangesNode.hpp"
 
 using namespace std::chrono_literals;
@@ -41,20 +41,21 @@ int main(int argc, char * argv[]) {
 
 	int imuOffsets[6] = {831, 1493, 1086, -155, -24, 19};
 
-	const uint8_t batteryInputNumbers[3] = { 3, 1, 6 };
-	const float batteryCoefficients[3] = { 734.4895, 340.7509, 214.1773 };
-	const uint8_t temperatureInputNumber = 5;
-	const float temperatureCoefficient = 564.7637;
+	// const uint8_t batteryInputNumbers[3] = { 3, 1, 6 };
+	// const float batteryCoefficients[3] = { 734.4895, 340.7509, 214.1773 };
+	// const uint8_t temperatureInputNumber = 5;
+	// const float temperatureCoefficient = 564.7637;
 
 	// front, back, top, left, right
-	/*const uint8_t vl53l0xPins[5] = { 67, 51, 66, 69, 50 };
+	/*
+	const uint8_t vl53l0xPins[5] = { 67, 51, 66, 69, 50 };
 	const uint8_t vl53l0xAddresses[5] = {
 		VL53L0X_ADDRESS_DEFAULT + 2,
 		VL53L0X_ADDRESS_DEFAULT + 4,
 		VL53L0X_ADDRESS_DEFAULT + 6,
 		VL53L0X_ADDRESS_DEFAULT + 10,
 		VL53L0X_ADDRESS_DEFAULT + 12
-	};*/
+	};
 
 	for (int i = 2; i <= argc; i+=2) {
 		if (!std::strcmp(argv[i-1], "-r")){
@@ -62,23 +63,23 @@ int main(int argc, char * argv[]) {
 		} else if (!std::strcmp(argv[i-1], "-w")){
 			baseWidth = atof(argv[i]);
 		}
-	}
+	}*/
 
-	auto batteryNode = std::make_shared<BatteryNode>(robotName, "sensor_battery", useIPC, 1000ms, batteryInputNumbers, batteryCoefficients);
+	// auto batteryNode = std::make_shared<BatteryNode>(robotName, "sensor_battery", useIPC, 1000ms, batteryInputNumbers, batteryCoefficients);
 	// auto dwmNode = std::make_shared<DWMNode>(robotName, "sensor_dwm1000", useIPC, 1000ms);
-	auto imuNode = std::make_shared<IMUNode>(robotName, "sensor_imu", useIPC, 20ms, 3000ms, imuOffsets);
-	auto temperatureNode = std::make_shared<TemperatureNode>(robotName, "sensor_temperature", useIPC, 2000ms, temperatureInputNumber, temperatureCoefficient);
+	auto imuNode = std::make_shared<IMUNode>(robotName, "sensor_imu", useIPC, 5ms, 3000ms, imuOffsets);
+	// auto temperatureNode = std::make_shared<TemperatureNode>(robotName, "sensor_temperature", useIPC, 2000ms, temperatureInputNumber, temperatureCoefficient);
 	// auto rangesNode = std::make_shared<RangesNode>(robotName, "sensor_ranges", useIPC, 20ms, vl53l0xPins, vl53l0xAddresses);
 	auto motorsNode = std::make_shared<MotorsControllerNode>(robotName, "motors_controller", useIPC, 10ms, wheelRadius, baseWidth);
 
-	setRTPriority();
+	// setRTPriority();
 	// return 0;
 
 	executor.add_node(motorsNode);
-	executor.add_node(batteryNode);
+	// executor.add_node(batteryNode);
 	// executor.add_node(dwmNode);
 	executor.add_node(imuNode);
-	executor.add_node(temperatureNode);
+	// executor.add_node(temperatureNode);
 	// executor.add_node(rangesNode);
 	executor.spin();
 
--- src/rys_base/launch/src/multithread.cpp
+++ src/rys_base/launch/src/multithread.cpp
@@ -5,11 +5,11 @@
 #include "rclcpp/rclcpp.hpp"
 
 #include "rys_motors_controller/MotorsControllerNode.hpp"
-#include "rys_sensor_battery/BatteryNode.hpp"
-#include "rys_sensor_dwm1000/DWMNode.hpp"
+// #include "rys_sensor_battery/BatteryNode.hpp"
+// #include "rys_sensor_dwm1000/DWMNode.hpp"
 #include "rys_sensor_imu/IMUNode.hpp"
-#include "rys_sensor_temperature/TemperatureNode.hpp"
-#include "rys_sensor_ranges/RangesNode.hpp"
+// #include "rys_sensor_temperature/TemperatureNode.hpp"
+// #include "rys_sensor_ranges/RangesNode.hpp"
 
 using namespace std::chrono_literals;
 
@@ -40,12 +40,13 @@ int main(int argc, char * argv[]) {
 
 	int imuOffsets[6] = {831, 1493, 1086, -155, -24, 19};
 
-	const uint8_t batteryInputNumbers[3] = { 3, 1, 6 };
-	const float batteryCoefficients[3] = { 734.4895, 340.7509, 214.1773 };
-	const uint8_t temperatureInputNumber = 5;
-	const float temperatureCoefficient = 564.7637;
+	// const uint8_t batteryInputNumbers[3] = { 3, 1, 6 };
+	// const float batteryCoefficients[3] = { 734.4895, 340.7509, 214.1773 };
+	// const uint8_t temperatureInputNumber = 5;
+	// const float temperatureCoefficient = 564.7637;
 
 	// front, back, top, left, right
+	/*
 	const uint8_t vl53l0xPins[5] = { 67, 51, 66, 69, 50 };
 	const uint8_t vl53l0xAddresses[5] = {
 		VL53L0X_ADDRESS_DEFAULT + 2,
@@ -54,20 +55,24 @@ int main(int argc, char * argv[]) {
 		VL53L0X_ADDRESS_DEFAULT + 10,
 		VL53L0X_ADDRESS_DEFAULT + 12
 	};
+	*/
 
-	auto batteryNode = std::make_shared<BatteryNode>(robotName, "sensor_battery", useIPC, 1000ms, batteryInputNumbers, batteryCoefficients);
+	// auto batteryNode = std::make_shared<BatteryNode>(robotName, "sensor_battery", useIPC, 1000ms, batteryInputNumbers, batteryCoefficients);
 	// auto dwmNode = std::make_shared<DWMNode>(robotName, "sensor_dwm1000", useIPC, 1000ms);
 	auto imuNode = std::make_shared<IMUNode>(robotName, "sensor_imu", useIPC, 20ms, 3000ms, imuOffsets);
-	auto temperatureNode = std::make_shared<TemperatureNode>(robotName, "sensor_temperature", useIPC, 2000ms, temperatureInputNumber, temperatureCoefficient);
+	// auto temperatureNode = std::make_shared<TemperatureNode>(robotName, "sensor_temperature", useIPC, 2000ms, temperatureInputNumber, temperatureCoefficient);
 	// auto rangesNode = std::make_shared<RangesNode>(robotName, "sensor_ranges", useIPC, 20ms, vl53l0xPins, vl53l0xAddresses);
 	auto motorsNode = std::make_shared<MotorsControllerNode>(robotName, "motors_controller", useIPC, 10ms, wheelRadius, baseWidth);
 
 	executor.add_node(motorsNode);
-	executor.add_node(batteryNode);
+	// executor.add_node(batteryNode);
 	// executor.add_node(dwmNode);
 	executor.add_node(imuNode);
-	executor.add_node(temperatureNode);
+	// executor.add_node(temperatureNode);
 	// executor.add_node(rangesNode);
+
+	setRTPriority();
+
 	executor.spin();
 
 	rclcpp::shutdown();
