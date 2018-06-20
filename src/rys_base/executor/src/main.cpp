bd8c3254e89d12c3bf9c6c882470a50686973e20
Dangield <dan.gield@gmail.com>
2018-06-20 19:40:26 +0200

    rys_executor/main: - added own qos communication profile - moved several variables in rollSender and steeringReceiver threads - created new remoteComm thread, that is singular replacement for rollSender and steeringReceiver threads, because it creates only one node that posses both subscribers and publishers CPU usage seem to drop by 10% while running rys_executor/main

--- src/rys_base/executor/src/main.cpp
+++ src/rys_base/executor/src/main.cpp
@@ -377,6 +377,13 @@ void steeringReceiver(bool& activate, std::mutex& m, bool& destroy, SteeringData
     sm.unlock();
     const std::string robotName = "rys";
     const std::string nodeName = "receiver";
+    const rmw_qos_profile_t qos = {
+        RMW_QOS_POLICY_HISTORY_KEEP_LAST,
+        1,
+        RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT,
+        RMW_QOS_POLICY_DURABILITY_VOLATILE,
+        false
+    };
 
     auto node = rclcpp::Node::make_shared(nodeName, robotName, false);
     auto steeringCallback = 
@@ -386,9 +393,7 @@ void steeringReceiver(bool& activate, std::mutex& m, bool& destroy, SteeringData
             precision = message->precision;
             newSteering = true;
         };
-    auto sub = node->create_subscription<rys_interfaces::msg::Steering>("/" + robotName + "/control/steering", steeringCallback);
-    rclcpp::executors::SingleThreadedExecutor ros_executor;
-    ros_executor.add_node(node);
+    auto sub = node->create_subscription<rys_interfaces::msg::Steering>("/" + robotName + "/control/steering", steeringCallback, qos);
 
     while(!destroy){
         m.lock();
@@ -396,7 +401,7 @@ void steeringReceiver(bool& activate, std::mutex& m, bool& destroy, SteeringData
             activate = false;
             m.unlock();
 
-            ros_executor.spin_some();
+            rclcpp::spin_some(node);
 
             if(newSteering){
                 sm.lock();
@@ -409,7 +414,7 @@ void steeringReceiver(bool& activate, std::mutex& m, bool& destroy, SteeringData
             }
 
             numOfRuns++;
-            if (numOfRuns > 5999) {
+            if (numOfRuns > 1199) {
                 previous = timeNow;
                 timeNow = std::chrono::high_resolution_clock::now();
                 auto loopTimeSpan = std::chrono::duration_cast<std::chrono::duration<float>>(timeNow - previous);
@@ -438,11 +443,25 @@ void rollSender(bool& activate, std::mutex& m, bool& destroy, IMU::ImuData& extD
 
     const std::string robotName = "rys";
     const std::string nodeName = "rollSender";
+    const rmw_qos_profile_t qos = {
+        RMW_QOS_POLICY_HISTORY_KEEP_LAST,
+        1,
+        RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT,
+        RMW_QOS_POLICY_DURABILITY_VOLATILE,
+        false
+    };
 
     auto node = rclcpp::Node::make_shared(nodeName, robotName, false);
-    auto pub = node->create_publisher<sensor_msgs::msg::Imu>("/" + robotName + "/sensor/imuInfrequent");
+    auto pub = node->create_publisher<sensor_msgs::msg::Imu>("/" + robotName + "/sensor/imuInfrequent", qos);
     IMU::ImuData localData;
     auto message = std::make_shared<sensor_msgs::msg::Imu>();
+    message->header.frame_id = "MPU6050";
+    for (int i = 0; i < 9; ++i) {
+        message->orientation_covariance[i] = 0;
+        message->angular_velocity_covariance[i] = 0;
+        message->linear_acceleration_covariance[i] = 0;
+    }
+    
 
     while(!destroy){
         m.lock();
@@ -456,7 +475,6 @@ void rollSender(bool& activate, std::mutex& m, bool& destroy, IMU::ImuData& extD
 
             // std::cout << localData.orientationQuaternion[1] << std::endl;
             message->header.stamp = node->now();
-            message->header.frame_id = "MPU6050";
             message->orientation.x = localData.orientationQuaternion[1];
             message->orientation.y = localData.orientationQuaternion[2];
             message->orientation.z = localData.orientationQuaternion[3];
@@ -467,11 +485,6 @@ void rollSender(bool& activate, std::mutex& m, bool& destroy, IMU::ImuData& extD
             message->linear_acceleration.x = localData.linearAcceleration[0];
             message->linear_acceleration.y = localData.linearAcceleration[1];
             message->linear_acceleration.z = localData.linearAcceleration[2];
-            for (int i = 0; i < 9; ++i) {
-                message->orientation_covariance[i] = 0;
-                message->angular_velocity_covariance[i] = 0;
-                message->linear_acceleration_covariance[i] = 0;
-            }
 
             pub->publish(message);
 
@@ -493,6 +506,112 @@ void rollSender(bool& activate, std::mutex& m, bool& destroy, IMU::ImuData& extD
     }
     rclcpp::shutdown();
     std::cout << name << ": I'm dying.." << std::endl;
+}
+
+void remoteComm(bool& activate, std::mutex& m, bool& destroy, IMU::ImuData& extData, std::mutex& dm, SteeringData& s, std::mutex& sm){
+    std::string name = "remoteComm";
+    pthread_setname_np(pthread_self(), name.c_str());
+    int numOfRuns = 0;
+    float frequency = 0;
+    std::chrono::time_point<std::chrono::high_resolution_clock> previous = std::chrono::high_resolution_clock::now();
+    std::chrono::time_point<std::chrono::high_resolution_clock> timeNow = std::chrono::high_resolution_clock::now();
+
+    float throttle = 0;
+    float rotation = 0;
+    int precision = 32;
+
+    float newSteering = false;
+    sm.lock();
+    s.throttle = throttle;
+    s.rotation = rotation;
+    s.precision = precision;
+    // std::cout << throttle << rotation << precision << std::endl;
+    sm.unlock();
+    const std::string robotName = "rys";
+    const std::string nodeName = "remoteComm";
+    const rmw_qos_profile_t qos = {
+        RMW_QOS_POLICY_HISTORY_KEEP_LAST,
+        1,
+        RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT,
+        RMW_QOS_POLICY_DURABILITY_VOLATILE,
+        false
+    };
+
+    auto node = rclcpp::Node::make_shared(nodeName, robotName, false);
+    auto steeringCallback = 
+        [&](const rys_interfaces::msg::Steering::SharedPtr message){
+            throttle = message->throttle;
+            rotation = message->rotation;
+            precision = message->precision;
+            newSteering = true;
+        };
+    auto sub = node->create_subscription<rys_interfaces::msg::Steering>("/" + robotName + "/control/steering", steeringCallback, qos);
+    auto pub = node->create_publisher<sensor_msgs::msg::Imu>("/" + robotName + "/sensor/imuInfrequent", qos);
+    IMU::ImuData localData;
+    auto message = std::make_shared<sensor_msgs::msg::Imu>();
+    message->header.frame_id = "MPU6050";
+    for (int i = 0; i < 9; ++i) {
+        message->orientation_covariance[i] = 0;
+        message->angular_velocity_covariance[i] = 0;
+        message->linear_acceleration_covariance[i] = 0;
+    }
+
+    while(!destroy){
+        m.lock();
+        if(activate){
+            activate = false;
+            m.unlock();
+
+            dm.lock();
+            localData = extData;
+            dm.unlock();
+
+            // std::cout << localData.orientationQuaternion[1] << std::endl;
+            message->header.stamp = node->now();
+            message->orientation.x = localData.orientationQuaternion[1];
+            message->orientation.y = localData.orientationQuaternion[2];
+            message->orientation.z = localData.orientationQuaternion[3];
+            message->orientation.w = localData.orientationQuaternion[0];
+            message->angular_velocity.x = localData.angularVelocity[0];
+            message->angular_velocity.y = localData.angularVelocity[1];
+            message->angular_velocity.z = localData.angularVelocity[2];
+            message->linear_acceleration.x = localData.linearAcceleration[0];
+            message->linear_acceleration.y = localData.linearAcceleration[1];
+            message->linear_acceleration.z = localData.linearAcceleration[2];
+
+            pub->publish(message);
+
+            rclcpp::spin_some(node);
+
+            if(newSteering){
+                sm.lock();
+                s.throttle = throttle;
+                s.rotation = rotation;
+                s.precision = precision;
+                // std::cout << throttle << rotation << precision << std::endl;
+                sm.unlock();
+                newSteering = false;
+            }
+
+            numOfRuns++;
+            if (numOfRuns > 1199) {
+                previous = timeNow;
+                timeNow = std::chrono::high_resolution_clock::now();
+                auto loopTimeSpan = std::chrono::duration_cast<std::chrono::duration<float>>(timeNow - previous);
+                float loopTime = loopTimeSpan.count();
+                frequency = numOfRuns/loopTime;
+                std::cout << name << " Frequency " << frequency << "Hz after " << numOfRuns << " messages." << std::endl;
+                numOfRuns = 0;
+            }
+
+        } else {
+            m.unlock();
+        }
+        std::this_thread::sleep_for(std::chrono::microseconds(100));
+    }
+    rclcpp::shutdown();
+    std::cout << name << ": I'm dying.." << std::endl;
+
 }
 
 int main(int argc, char * argv[]){
@@ -543,27 +662,36 @@ int main(int argc, char * argv[]){
 
     exec->addExec(std::ref(motors_mutex), std::ref(motors_bool), std::chrono::milliseconds(10));
 
-    bool steering_bool = false;
-    std::mutex steering_mutex;
-    std::thread t5(steeringReceiver, std::ref(steering_bool), std::ref(steering_mutex), std::ref(destruct),
+    // bool steering_bool = false;
+    // std::mutex steering_mutex;
+    // std::thread t5(steeringReceiver, std::ref(steering_bool), std::ref(steering_mutex), std::ref(destruct),
+    //                 std::ref(steering_data), std::ref(steering_data_mutex));
+
+    // exec->addExec(std::ref(steering_mutex), std::ref(steering_bool), std::chrono::milliseconds(50));
+
+    // bool roll_sender_bool = false;
+    // std::mutex sender_mutex;
+    // std::thread t6(rollSender, std::ref(roll_sender_bool), std::ref(sender_mutex), std::ref(destruct),
+    //                 std::ref(imuData), std::ref(imuData_mutex));
+
+    // exec->addExec(std::ref(sender_mutex), std::ref(roll_sender_bool), std::chrono::milliseconds(50));
+
+    bool remoteComm_bool = false;
+    std::mutex remoteComm_mutex;
+    std::thread t7(remoteComm, std::ref(remoteComm_bool), std::ref(remoteComm_mutex), std::ref(destruct),
+                    std::ref(imuData), std::ref(imuData_mutex),
                     std::ref(steering_data), std::ref(steering_data_mutex));
 
-    exec->addExec(std::ref(steering_mutex), std::ref(steering_bool), std::chrono::milliseconds(10));
+    exec->addExec(std::ref(remoteComm_mutex), std::ref(remoteComm_bool), std::chrono::milliseconds(50));
 
-    bool roll_sender_bool = false;
-    std::mutex sender_mutex;
-    std::thread t6(rollSender, std::ref(roll_sender_bool), std::ref(sender_mutex), std::ref(destruct),
-                    std::ref(imuData), std::ref(imuData_mutex));
-
-    exec->addExec(std::ref(sender_mutex), std::ref(roll_sender_bool), std::chrono::milliseconds(50));
- 
     exec->list();
     exec->spin();
     t1.join();
     t2.join();
     t3.join();
     t4.join();
-    t5.join();
-    t6.join();
+    // t5.join();
+    // t6.join();
+    t7.join();
     delete exec;
 }
